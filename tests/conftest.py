e7c96e06984fe61035bdb2dcf1f5fb7534e80099
Likhon <likh.deshi@gmail.com>
2024-11-20 17:38:16 +0600

    Close the conn

--- tests/conftest.py
+++ tests/conftest.py
@@ -109,3 +109,4 @@ def destination() -> Generator[DestinationPostgreSQL, None, None]:
 
     # Drop the table after the session ends
     dest.query(f"DROP TABLE IF EXISTS {dest.TABLE_NAME};")
+    dest.disconnect()
